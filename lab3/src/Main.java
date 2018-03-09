5ac26fa325042bef480440f3d039b6f7a0b8b64b
e-dzia <226000@student.pwr.edu.pl>
2018-03-09 15:21:25 +0100

    l3: backup before major changes

--- lab3/src/Main.java
+++ lab3/src/Main.java
@@ -1,3 +1,6 @@
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+import java.lang.ref.ReferenceQueue;
 import java.util.*;
 import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
@@ -5,13 +8,15 @@ import java.util.concurrent.Executors;
 
 public class Main {
     public static  ExecutorService exec = Executors.newFixedThreadPool(5);
-    public static Map<Integer,Collection> collectionMap = Collections.synchronizedMap(new WeakHashMap<>());
+    public static ReferenceQueue<Collection> referenceQueue = new ReferenceQueue<>();
+    public static Map<Integer,Collection> collectionMap
+            = Collections.synchronizedMap(new WeakHashMap<Integer,Collection>());
 
     private static class Task implements Runnable {
         static Random generator = new Random();
 
         public void run() {
-            for (int i = 0; i < 10; i++){
+            for (int i = 0; i < 5; i++){
                 int grain = generator.nextInt(4000);
                 if (!collectionMap.containsKey(grain)){
                     collectionMap.put(grain, new Collection(grain));
@@ -20,13 +25,16 @@ public class Main {
                 synchronized (System.out) {
                     System.out.println(grain + " " + Thread.currentThread().getId());
                     System.out.println("Min: " + min + " " + Thread.currentThread().getId());
+                    System.out.println(collectionMap.keySet().size());
                 }
                 try {
-                    Thread.sleep(1000);
+                    Thread.sleep(500);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
+            System.gc();
+            System.out.println("Col: " + Collection.finalizes);
         }
     }
 
@@ -34,8 +42,9 @@ public class Main {
         for (int i = 0; i < 5; i++){
             exec.execute(new Task());
         }
-        do{
+        if (!exec.isShutdown()) {
+            exec.shutdown();
             System.out.println("Col: " + Collection.finalizes);
-        }while(exec.isShutdown());
+        }
     }
 }
