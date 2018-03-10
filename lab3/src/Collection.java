a18f786fd4e7d313f09f8529b577cd56819fd51c
e-dzia <theladyofthegallifrey@gmail.com>
2018-03-10 16:12:56 +0100

    l3: works well

--- lab3/src/Collection.java
+++ lab3/src/Collection.java
@@ -5,14 +5,28 @@ public class Collection {
     ArrayList<Double> collection = new ArrayList<>();
 
     public Collection(int grain){
-        long size = 5000000 + grain*100;
-        for (int i = 0; i < size; i++){
-            collection.add(grain * 200 + 2000.0 * i + 0.5);
+        long size = 3000000 + grain*100;
+        try {
+            for (int i = 0; i < size; i++) {
+                collection.add(grain * 200 + 2000.0 * i + 0.5);
+            }
+        }
+        catch (OutOfMemoryError e){
+            System.out.println("\t\tOutOfMemoryError: " + grain);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e1) {
+                e1.printStackTrace();
+            }
+            collection.clear();
+            for (int i = 0; i < size; i++) {
+                collection.add(grain * 200 + 2000.0 * i + 0.5);
+            }
         }
     }
 
     public void finalize(){
-        //System.out.println("\t\t\t\t%Usuwam: " + (collection.size()-1000000)/100);
+        System.out.println("\t\t\t\tUsuwam: " + (collection.size()-3000000)/100);
         deletedObjects++;
     }
 
--- lab3/src/Main.java
+++ lab3/src/Main.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.lang.ref.ReferenceQueue;
+import java.lang.ref.SoftReference;
 import java.lang.ref.WeakReference;
 import java.util.*;
 import java.util.concurrent.ExecutorService;
@@ -8,10 +9,11 @@ import java.util.concurrent.Executors;
 public class Main {
     static int numberOfThreads = 5;
     static int numberOfIterations = 5;
-    public static  ExecutorService exec = Executors.newFixedThreadPool(numberOfThreads);
-    public static ReferenceQueue<Collection> referenceQueue = new ReferenceQueue<>();
-    public static Map<Integer,WeakReference<Collection>> collectionMap = Collections.synchronizedMap(new HashMap<>());
-    public static ArrayList<Boolean> allFinished = new ArrayList<Boolean>();
+
+    static  ExecutorService exec = Executors.newFixedThreadPool(numberOfThreads);
+    static ReferenceQueue<Collection> referenceQueue = new ReferenceQueue<>();
+    static Map<Integer,SoftReference<Collection>> collectionMap = Collections.synchronizedMap(new HashMap<>());
+    static ArrayList<Boolean> allFinished = new ArrayList<>();
 
     private static class Task implements Runnable {
         static Random generator = new Random();
@@ -20,38 +22,39 @@ public class Main {
         public void run() {
             try {
                 output = new PrintWriter("output_" + Thread.currentThread().getId()+".txt", "UTF-8");
-            } catch (FileNotFoundException | UnsupportedEncodingException e) {
+            } catch (FileNotFoundException | UnsupportedEncodingException e){
                 e.printStackTrace();
             }
+
             boolean madeNewCollection;
 
             for (int i = 0; i < numberOfIterations; i++){
-                Integer grain = generator.nextInt(4000);
+                Integer grain = generator.nextInt(50);
                 madeNewCollection = false;
                 Double min = -1.0;
                 synchronized (collectionMap){
                     if (!collectionMap.containsKey(grain) || collectionMap.get(grain).isEnqueued()){
                         System.out.println("\tTworze kolekcje:\t" + grain);
                         output.write("\tTworze kolekcje:\t" + grain + "\n");
-                        collectionMap.put(grain, new WeakReference<>(new Collection(grain),referenceQueue));
+                        collectionMap.put(grain, new SoftReference<>(new Collection(grain),referenceQueue));
                         madeNewCollection = true;
                     }
                     try {
                         min = collectionMap.get(grain).get().countMinimum();
                     }
                     catch (NullPointerException e1){
-                        System.out.println("Null pointer:" + grain);
+                        System.out.println("\t\tNull pointer: " + grain);
                     }
                 }
-                synchronized (System.out) {
-                    System.out.println("Watek: " + Thread.currentThread().getId() +
+                synchronized (System.out){
+                    System.out.println("Watek: " + Thread.currentThread().getId() + ", Iteracja: " + i +
                             ", Ziarno: " + grain + ", Min: " + min + ", Stworzenie kolekcji: " + madeNewCollection);
                     //System.out.println(collectionMap.size());
                 }
                 output.write("Ziarno: " + grain + ", Min: " + min +"\n");
                 try {
                     Thread.sleep(1000);
-                } catch (InterruptedException e) {
+                } catch (InterruptedException e){
                     e.printStackTrace();
                 }
             }
@@ -59,7 +62,6 @@ public class Main {
             synchronized (allFinished){
                 allFinished.remove(0);
             }
-
         }
     }
 
@@ -72,7 +74,6 @@ public class Main {
             Thread.sleep(1000);
         }
         exec.shutdown();
-        System.out.println("Col: " + Collection.deletedObjects);
-
+        System.out.println("Usuniete kolekcje: " + Collection.deletedObjects);
     }
 }
