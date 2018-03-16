9531ce40d3c744b3470efaec5eef2812d70f576c
e-dzia <theladyofthegallifrey@gmail.com>
2018-03-16 14:13:23 +0100

    l4: succeded in copying ClassLoader from the Internet

--- lab4/src/Main.java
+++ lab4/src/Main.java
@@ -1,12 +1,28 @@
+import java.io.IOException;
+import java.lang.reflect.*;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException, IOException {
         GraphArray graphArray = new GraphArray();
         graphArray.loadFromFile("tsp10.txt");
         graphArray.print();
 
         //TODO:zaladowac greedy, bruteforce i bestof
+        Object o = new GraphArray();
+        Class c = o.getClass();
 
+
+        //System.in.read();
+
+        ClassLoader classLoader = new CCLoader(ClassLoader.getSystemClassLoader());
+        Class bruteForce = classLoader.loadClass("BruteForceAlgorithm");
+        System.out.println(bruteForce.getClassLoader());
+
+        Class[] theInterfaces = bruteForce.getInterfaces();
+        for (int i = 0; i < theInterfaces.length; i++) {
+            String interfaceName = theInterfaces[i].getName();
+            System.out.println(interfaceName);
+        }
     }
 }
