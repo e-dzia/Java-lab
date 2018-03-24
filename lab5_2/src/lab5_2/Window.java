836a49a1c1350da9a37956417636d089ddc339aa
e-dzia <theladyofthegallifrey@gmail.com>
2018-03-24 21:57:19 +0100

    l5: finally works!

--- lab5_2/src/lab5_2/Window.java
+++ lab5_2/src/lab5_2/Window.java
@@ -1,42 +1,19 @@
 package lab5_2;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
+import javax.swing.*;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
+public class Window {
+    private JPanel panel1;
 
-public class Window extends JFrame {
+    public static void createAndShowGUI() {
+        JFrame frame = new JFrame("JavaBeans");
+        frame.setSize(500, 500);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-	private JPanel contentPane;
+        frame.setContentPane(new Window().panel1);
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Window frame = new Window();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
-	 * Create the frame.
-	 */
-	public Window() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
-		setContentPane(contentPane);
-	}
+        frame.pack();
+        frame.setVisible(true);
+    }
 
 }
