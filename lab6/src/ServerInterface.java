6f4d614aa008afe5e6289cef5973dd80e74ff7cf
e-dzia <theladyofthegallifrey@gmail.com>
2018-05-06 15:26:38 +0200

    l09: started

--- lab6/src/ServerInterface.java
+++
@@ -1,8 +0,0 @@
-import java.rmi.Remote;
-import java.util.Vector;
-
-public interface ServerInterface extends Remote {
-    int registerNode(PeerInterface peer) throws java.rmi.RemoteException;
-    boolean uploadFile(String filename, byte[] data) throws java.rmi.RemoteException;
-    Vector<PeerInterface> getPeersForFile(String filename) throws java.rmi.RemoteException;
-}
--- lab8-axis2-s/src/server/Node.java
+++
@@ -1,8 +0,0 @@
-package server;
-
-import java.util.ArrayList;
-
-public class Node {
-    public String name;
-    public ArrayList<Message> messages = new ArrayList<>();
-}
